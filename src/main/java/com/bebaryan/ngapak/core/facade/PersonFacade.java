a03e41450d9b329d0671662af16d41fd3b811f90
BHasani <abdul.kadir.hasani@basajans.com>
2017-07-22 14:28:45 +0700

    start

---
+++ src/main/java/com/bebaryan/ngapak/core/facade/PersonFacade.java
@@ -0,0 +1,31 @@
+package com.bebaryan.ngapak.core.facade;
+
+import com.bebrayan.ngapak.core.entity.Person;
+import com.bebrayan.ngapak.core.facade.AbstractDatabaseFacade;
+import com.bebrayan.ngapak.core.facade.PersonFacadeLocal;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hasani
+ */
+@Stateless
+public class PersonFacade extends AbstractDatabaseFacade<Person> implements PersonFacadeLocal {
+    
+    @PersistenceContext(unitName = "BebrayanLab")
+    private EntityManager em;
+
+    public PersonFacade() {
+        super(Person.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
